0223b486c14252b4817b10e726db6707c3dfa641
vaibhav-saini <36201580+vaibhav-saini@users.noreply.github.com>
2018-03-17 11:24:19 +0530

    Add files via upload

---
+++ order_serach.js
@@ -0,0 +1,95 @@
+/*================== order list search with input ==============*/
+  function search_name() {
+                var srchh = document.getElementById('search_nam').value;
+                var demo = document.getElementById('demo');
+                var classs= demo.getElementsByClassName("col_box")
+                for(var i = 0; i < classs.length; i++){
+                    var loop_i = classs[i];
+                        //console.log(loop_i)
+                    var node_list = loop_i.childNodes[2]
+                  // console.log(node_list)
+                    var node_span = node_list.getElementsByTagName('span')[0]
+                       // console.log(node_span)
+                    var col_boot = document.getElementsByClassName('col-sm-3')
+                    if(node_span.innerHTML.indexOf(srchh) > -1){
+
+                       col_boot[i].style.display="block";
+                    }
+                 else{
+                             col_boot[i].style.display="none";
+                    }
+                     
+                }
+                }
+               
+  /*=============== select =================*/
+    function abc(){
+         var x = document.getElementById('select_id').value
+         var demo = document.getElementById('demo');
+                var classs= demo.getElementsByClassName("col_box")
+                for(var i = 0; i < classs.length; i++){
+                    var loop_i = classs[i];
+                        //console.log(loop_i)
+                    var node_list = loop_i.childNodes[3]
+//console.log(node_list)
+                    var node_span = node_list.getElementsByTagName('span')[0]
+                       // console.log(node_span)
+                    var col_boot = document.getElementsByClassName('col-sm-3')
+                    if(node_span.innerHTML.indexOf(x) > -1){
+                       // console.log(x);
+                       col_boot[i].className = "col-sm-3 block"
+                    }
+                    else{
+                             col_boot[i].classList.remove("block") 
+                    }
+                    
+                     
+                }
+                }          
+    
+      /*=============== driver =================*/
+    function driver(){
+         var y = document.getElementById('driver_sel').value
+         var demo = document.getElementById('demo');
+                var classs= demo.getElementsByClassName("col_box")
+                for(var i = 0; i < classs.length; i++){
+                    var loop_i = classs[i];
+                        //console.log(loop_i)
+                    var node_list = loop_i.childNodes[4]
+//console.log(node_list)
+                    var node_span = node_list.getElementsByTagName('span')[0]
+                       // console.log(node_span)
+                    var col_boot = document.getElementsByClassName('col-sm-3')
+                    if(node_span.innerHTML.indexOf(y) > -1){
+                       // console.log(x);
+                       col_boot[i].className = "col-sm-3 block"
+                    }
+                    else{
+                             col_boot[i].classList.remove("block") 
+                    }
+                    
+                     
+                }
+                }  
+
+/*======reset============*/
+    
+    function reset(){
+       // var reset_value = document.getElementById('search_nam').value
+      //  console.log(reset_value="")
+       document.getElementById('search_nam').value = ""
+       var sel = document.getElementById('driver_sel').value
+       var sel_status = document.getElementById('select_id').value
+      if(sel != "reset" || sel_status != "reset"){
+            document.getElementById('driver_sel').selectedIndex = 0;
+            document.getElementById('select_id').selectedIndex = 0;
+      }
+        
+        var demo = document.getElementById('demo');
+         var classs= demo.getElementsByClassName("col_box");
+          var col_boot = document.getElementsByClassName('col-sm-3')
+           for(var i = 0; i < classs.length; i++){
+                col_boot[i].classList.remove("block") 
+                 col_boot[i].style.display="block";
+           }
+        }
