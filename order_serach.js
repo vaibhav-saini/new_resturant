697353177801be9173da465b2217c87a6f2915cd
vaibhav-saini <36201580+vaibhav-saini@users.noreply.github.com>
2018-03-19 14:56:49 +0530

    Add files via upload

--- order_serach.js
+++ order_serach.js
@@ -3,23 +3,27 @@
                 var srchh = document.getElementById('search_nam').value;
                 var demo = document.getElementById('demo');
                 var classs= demo.getElementsByClassName("col_box")
+				var col_boot = document.getElementsByClassName('col-sm-3')
+				//console.log(classs)
                 for(var i = 0; i < classs.length; i++){
-                    var loop_i = classs[i];
-                        //console.log(loop_i)
-                    var node_list = loop_i.childNodes[2]
-                  // console.log(node_list)
-                    var node_span = node_list.getElementsByTagName('span')[0]
-                       // console.log(node_span)
-                    var col_boot = document.getElementsByClassName('col-sm-3')
-                    if(node_span.innerHTML.indexOf(srchh) > -1){
-
-                       col_boot[i].style.display="block";
-                    }
-                 else{
-                             col_boot[i].style.display="none";
-                    }
-                     
-                }
+                 var list_span =  classs[i].getElementsByTagName('span')
+					console.log(srchh)
+					for(var j = 0; j<list_span.length; j++ ){
+						if(list_span[j].innerHTML == srchh){
+							col_boot[i].className = "col-sm-3 block"
+							list_span[j].className = "bold"
+						}
+						else if(list_span[j].innerHTML != srchh){
+							console.log('sad');
+							col_boot[i].classList.remove = "block"
+							list_span[j].classList.remove = "bold"
+						}
+						
+					}
+				   
+                 
+                  
+                  }
                 }
                
   /*=============== select =================*/
@@ -31,18 +35,20 @@
                     var loop_i = classs[i];
                         //console.log(loop_i)
                     var node_list = loop_i.childNodes[3]
-//console.log(node_list)
+					//console.log(node_list)
                     var node_span = node_list.getElementsByTagName('span')[0]
                        // console.log(node_span)
                     var col_boot = document.getElementsByClassName('col-sm-3')
                     if(node_span.innerHTML.indexOf(x) > -1){
                        // console.log(x);
-                       col_boot[i].className = "col-sm-3 block"
-                         node_span.className = "bold"
+                      // col_boot[i].className = "col-sm-3 block"
+                        node_span.className = "bold"
+						col_boot[i].style.display="block";
                     }
                     else{
-                            col_boot[i].classList.remove("block") 
-                           node_span.classList.remove("bold")
+                          //  col_boot[i].classList.remove("block") 
+                          node_span.classList.remove("bold")
+						  col_boot[i].style.display="none";
                     }
                     
                      
@@ -65,12 +71,14 @@
                     var col_boot = document.getElementsByClassName('col-sm-3')
                     if(node_span.innerHTML.indexOf(y) > -1){
                        // console.log(x);
-                       col_boot[i].className = "col-sm-3 block"
-                        node_span.className = "bold"
+                   //    col_boot[i].className = "col-sm-3 block"
+                       node_span.className = "bold"
+					col_boot[i].style.display="block";
                     }
                     else{
-                             col_boot[i].classList.remove("block") 
-                         node_span.classList.remove("bold")
+                           //  col_boot[i].classList.remove("block") 
+                        node_span.classList.remove("bold")
+						 col_boot[i].style.display="none";
                     }
                     
                      
@@ -105,9 +113,9 @@
                     //console.log(node_list)
                     var node_span = node_list.getElementsByTagName('span')[0]
                     var node_span_status = node_list_status.getElementsByTagName('span')[0]
-                       node_span.classList.remove("bold")
+                     node_span.classList.remove("bold")
                        node_span_status.classList.remove("bold")
-                    col_boot[i].classList.remove("block") 
+                   // col_boot[i].classList.remove("block") 
                  col_boot[i].style.display="block";
                
            }
